a2f2c3d9185d3856c67eac7bce9a8d8350f5010e
Artyom <tsanda.artyom@gmail.com>
2016-03-30 17:48:58 +0300

    changes int AuthorizationTest fixed

--- JavaFiddle-Web/src/test/java/ru/javafiddle/tests/AuthorizationTest.java
+++ JavaFiddle-Web/src/test/java/ru/javafiddle/tests/AuthorizationTest.java
@@ -7,6 +7,8 @@ import org.junit.runners.JUnit4;
 import ru.javafiddle.tests.utils.SystemOutFilter;
 
 
+import javax.ws.rs.core.Response;
+
 import static com.jayway.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.containsString;
 
@@ -19,21 +21,60 @@ public class AuthorizationTest {
     private final String HOME_PAGE_URL = "https://localhost:8181/javaFiddle";
     private final String EXICTING_USER_NICKNAME = "atsanda";
     private final String EXICTING_USER_PASSWORD = "03061995";
-    private final FormAuthConfig formAuthConfig = new FormAuthConfig("j_security_check", "j_username", "j_username");
+    private final FormAuthConfig formAuthConfig = new FormAuthConfig("j_security_check", "j_username", "j_password");
+
 
     @Test
     public void shouldLoadLoginPage() {
 
-        given().
+        com.jayway.restassured.response.Response r = given().
                 filter(new SystemOutFilter()).
                 relaxedHTTPSValidation().
+                auth().form(EXICTING_USER_NICKNAME, EXICTING_USER_PASSWORD, formAuthConfig).
         expect().
                 body(containsString("j_security_check")).
                 body(containsString("j_username")).
-                body(containsString("j_username")).
+                body(containsString("j_password")).
                 statusCode(200).
         when().
                 get(HOME_PAGE_URL);
+
+        System.out.println(r.body());
+
+    }
+
+    @Test
+    public void shouldLoadHomePage() {
+        com.jayway.restassured.response.Response r = given().
+                filter(new SystemOutFilter()).
+                relaxedHTTPSValidation().
+                auth().form(EXICTING_USER_NICKNAME, EXICTING_USER_PASSWORD, formAuthConfig).
+        expect().
+                body(containsString("j_security_check")).
+                body(containsString("j_username")).
+                body(containsString("j_password")).
+                statusCode(200).
+        when().
+                get(HOME_PAGE_URL + "/fiddle/users");
+
+        System.out.println(r.body());
+    }
+
+    @Test
+    public void shouldDeleteUser() {
+        com.jayway.restassured.response.Response r = given().
+                filter(new SystemOutFilter()).
+                relaxedHTTPSValidation().
+                auth().form(EXICTING_USER_NICKNAME, EXICTING_USER_PASSWORD, formAuthConfig).
+        expect().
+                body(containsString("j_security_check")).
+                body(containsString("j_username")).
+                body(containsString("j_password")).
+                statusCode(200).
+        when().
+                delete(HOME_PAGE_URL + "/fiddle/users/newUser");
+
+        System.out.println(r.body());
     }
 
 }
