3b45674910317c4c11e6f1a7f5f1cd215632b9bf
skotti <anastasiya.ruznaskaya@frtk.ru>
2016-04-15 00:45:02 +0400

    nope

--- JavaFiddle-ejb/src/test/java/ProjectBeanTest.java
+++ JavaFiddle-ejb/src/test/java/ProjectBeanTest.java
@@ -37,63 +37,66 @@ public class ProjectBeanTest {
         System.out.println("Test EJBContainer is closed" );
     }
 
-//!TODO
-//    @org.junit.Test
-//    public void testProjectOperations() throws UnsupportedEncodingException, NoSuchAlgorithmException, InstantiationException, IllegalAccessException {
-//        UserBean userBean = null;
-//        GroupBean groupBean = null;
-//        ProjectBean projectBean = null;
-//        HashBean hashBean = null;
-//        AccessBean accessBean = null;
-//        UserGroupBean userGroupBean = null;
-//
-//        try {
-//            projectBean = (ProjectBean) context.lookup("java:global/JavaFiddle-ejb/ProjectBean");
-//            userBean = (UserBean) context.lookup("java:global/JavaFiddle-ejb/UserBean");
-//            groupBean = (GroupBean) context.lookup("java:global/JavaFiddle-ejb/GroupBean");
-//            hashBean = (HashBean) context.lookup("java:global/JavaFiddle-ejb/HashBean");
-//            accessBean = (AccessBean) context.lookup("java:global/JavaFiddle-ejb/AccessBean");
-//            userGroupBean = (UserGroupBean) context.lookup("java:global/JavaFiddle-ejb/UserGroupBean");
-//
-//        } catch (NamingException ex) {
-//            System.out.println("Unable to initialize UserBean instance: " + ex);
-//        }
-//        Assert.assertNotNull(userBean);
-//
-//
-//        initialize(userBean, groupBean, accessBean, userGroupBean);
-//        Project project = new Project("first_proj", null);
-//        project = projectBean.createProject(1,project);
-//
-//        Group g = groupBean.getGroupByGroupId(1);
-//
-//        Assert.assertNotNull(project);
-//        for(Project p: projectBean.getProjects(g)){
-//            Assert.assertEquals("Not only one entity was added", "first_proj", p.getProjectName());
-//        }
-//
-//        Project project2 = new Project("first_proj", null);
-//        //project2.setHash(new Hash());
-//        project2 = projectBean.createProject(1,project2);
-//        Assert.assertNotNull(project2);
-//        Assert.assertFalse("Hashes are identical", project.getHash().getHash().equals(project2.getHash().getHash()));
-//
-//        //getUserProjects()
-//        User user = userBean.getUser("skotti");
-//        List<String> projects = projectBean.getUserProjects(user);
-//        Assert.assertNotNull("No projects", projects);
-//
-//        //projectBean.changeProjectName(project2.getHash().getHash(),"second_proj");
-//        Project newProject = new Project("second_proj", null);
-//        newProject.setHash(project2.getHash());
-//        Assert.assertTrue("The name of the project was not changed", projectBean.updateProject(newProject).getProjectName().equals("second_proj"));
-//        projectBean.deleteProject(project2.getHash().getHash());
-//        Assert.assertNull(hashBean.getHash(2));
-//        Assert.assertNull("The project was not deleted", projectBean.getProjectByProjectHash(project2.getHash().getHash()));
-//
-//
-//
-//    }
+
+    @org.junit.Test
+    public void testProjectOperations() throws UnsupportedEncodingException, NoSuchAlgorithmException, InstantiationException, IllegalAccessException {
+        UserBean userBean = null;
+        GroupBean groupBean = null;
+        ProjectBean projectBean = null;
+        HashBean hashBean = null;
+        AccessBean accessBean = null;
+        UserGroupBean userGroupBean = null;
+
+       try {
+            projectBean = (ProjectBean) context.lookup("java:global/classes/ProjectBean");
+            userBean = (UserBean) context.lookup("java:global/classes/UserBean");
+             groupBean = (GroupBean) context.lookup("java:global/classes/GroupBean");
+            hashBean = (HashBean) context.lookup("java:global/classes/HashBean");
+            accessBean = (AccessBean) context.lookup("java:global/classes/AccessBean");
+            userGroupBean = (UserGroupBean) context.lookup("java:global/classes/UserGroupBean");
+
+        } catch (NamingException ex) {
+            System.out.println("Unable to initialize UserBean instance: " + ex);
+        }
+        Assert.assertNotNull(userBean);
+
+
+        initialize(userBean, groupBean, accessBean, userGroupBean);
+        Group g = groupBean.getGroupByGroupId(1);
+        Project project = new Project("first_proj", null);
+        project.setGroup(g);
+        project = projectBean.createProject(project);
+
+
+
+        Assert.assertNotNull(project);
+        for(Project p: projectBean.getProjects(g)){
+            Assert.assertEquals("Not only one entity was added", "first_proj", p.getProjectName());
+        }
+
+        Project project2 = new Project("first_proj", null);
+        project2.setGroup(g);
+        //project2.setHash(new Hash());
+         project2 = projectBean.createProject(project2);
+        Assert.assertNotNull(project2);
+        Assert.assertFalse("Hashes are identical", project.getHash().getHash().equals(project2.getHash().getHash()));
+
+        //getUserProjects()
+        User user = userBean.getUser("skotti");
+        List<String> projects = projectBean.getUserProjects(user);
+        Assert.assertNotNull("No projects", projects);
+
+        //projectBean.changeProjectName(project2.getHash().getHash(),"second_proj");
+        Project newProject = new Project("second_proj", null);
+        newProject.setHash(project2.getHash());
+        Assert.assertTrue("The name of the project was not changed", projectBean.updateProject(newProject).getProjectName().equals("second_proj"));
+        projectBean.deleteProject(project2.getHash().getHash());
+        Assert.assertNull(hashBean.getHash(2));
+        Assert.assertNull("The project was not deleted", projectBean.getProjectByProjectHash(project2.getHash().getHash()));
+
+
+
+    }
 
     private void initialize(UserBean userBean, GroupBean groupBean, AccessBean accessBean, UserGroupBean userGroupBean) throws UnsupportedEncodingException, NoSuchAlgorithmException {
 
