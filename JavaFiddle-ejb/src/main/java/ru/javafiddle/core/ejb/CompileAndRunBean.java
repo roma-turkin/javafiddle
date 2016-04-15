0bd1f5090dcfd463bc320e23f6e19d022c261a2d
Кудашов Аркадий <ark100295@yandex.ru>
2016-04-15 03:05:21 +0300

    Some corrections

--- JavaFiddle-ejb/src/main/java/ru/javafiddle/core/ejb/CompileAndRunBean.java
+++ JavaFiddle-ejb/src/main/java/ru/javafiddle/core/ejb/CompileAndRunBean.java
@@ -2,11 +2,13 @@ package ru.javafiddle.core.ejb;
 
 import org.apache.log4j.BasicConfigurator;
 import org.apache.log4j.Logger;
+import org.springframework.util.StringUtils;
 import ru.javafiddle.jpa.entity.File;
 import ru.ncedu.dynamic.ByteArrayResource;
 import ru.ncedu.dynamic.DynamicCompiler;
 import ru.ncedu.dynamic.StringJavaFileObject;
 
+import javax.ejb.EJB;
 import javax.ejb.Stateless;
 import javax.inject.Named;
 import javax.persistence.EntityManager;
@@ -16,6 +18,8 @@ import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
+import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -26,54 +30,173 @@ import java.util.List;
 @Named(value = "compileAndRunBean")
 public class CompileAndRunBean extends DynamicCompiler {
 
-    @PersistenceContext
+    @PersistenceContext(name = "JFPersistenceUnit")
     EntityManager em;
+
+    @EJB
+    FileBean fileBean;
 
 
     public static final String DEFAULT_PACKAGE_PREFFIX = "";
     private static final Logger LOG = Logger.getLogger(CompileAndRunBean.class);
 
-    public void init(ClassLoader parentLoader) {
-        try {
-            super.init(parentLoader);
-        } catch (Exception e) {
-            LOG.error("Exception", e);
+    public enum FileType {
+        CLASS(8),
+        INTERFACE(9),
+        EXCEPTION(10),
+        RUNNABLE(11),
+        ENUM(12),
+        ANNOTATION(13);
+
+        private int type;
+
+        FileType(int type) {
+            this.type = type;
+        }
+
+        public int getType() {
+            return type;
+        }
+
+        public static boolean isFile(int type) {
+            for (FileType fileType : FileType.values()) {
+                if (fileType.getType() == type) {
+                    return true;
+                }
+            }
+            return false;
         }
     }
 
+    private final int SOURCES = 3;
+
+
+
+    public void init(ClassLoader parentLoader) throws Exception {
+        super.init(parentLoader);
+    }
+
     //!TODO
-    public String compile(String projectHash) {
+    public String compile(String projectHash) throws Exception {
         ByteArrayOutputStream baos = new ByteArrayOutputStream();
         PrintStream printStream = new PrintStream(baos);
-        PrintStream old = System.out;
-        System.setOut(printStream);
-        BasicConfigurator.configure();
-        List<File> sources;
-        FileBean fileBean = new FileBean();
-        sources = fileBean.getProjectFiles(projectHash);
-        try {
+        synchronized (System.out) {
+            PrintStream old = System.out;
+            System.setOut(printStream);
+            BasicConfigurator.configure();
+            List<File> sources;
+            //FileBean fileBean = new FileBean();
+            sources = fileBean.getProjectFiles(projectHash);
             init(ClassLoader.getSystemClassLoader());
-        } catch (NullPointerException e) {
-            LOG.error("NullPointerException in getSystemClassLoader()", e);
+            List<SimpleJavaFileObject> userSources = constructResources(sources);
+            String message = "";
+            try {
+                message = compileToClass(userSources);
+            } catch (Exception e) {
+                LOG.error("Exception, can't compile", e);
+            }
+            LOG.info(message);
+            System.out.flush();
+            System.setOut(old);
         }
+        return baos.toString();
+    }
+
+    //!TODO
+    public String run(String projectHash) {
+        List<File> sources;
+        //FileBean fileBean = new FileBean();
+        sources = fileBean.getProjectFiles(projectHash);
+        String mes = "";
+        if (StringUtils.isEmpty(getMessage())) {
+            Method mainMeth = findMain(sources, getClassLoader());
+            String[] arguments = {};
+            try {
+                ByteArrayOutputStream baos = new ByteArrayOutputStream();
+                PrintStream printStream = new PrintStream(baos);
+                synchronized (System.out) {
+                    PrintStream old = System.out;
+                    System.setOut(printStream);
+                    mainMeth.invoke(null, (Object) arguments);
+                    System.out.flush();
+                    System.setOut(old);
+                }
+                mes = baos.toString();
+            } catch (InvocationTargetException | IllegalAccessException e) {
+                LOG.error(e.getMessage(), e);
+            }
+        }
+        return mes;
+    }
+
+    public Method findMain(List<File> sources, final ClassLoader clazzLoader) {
+        int count = 0;
+        Class clazz;
+        Method mainMeth = null;
+        for (int i = 0; i < sources.size(); i++) {
+            File file = sources.get(i);
+            int typeId = file.getType().getTypeId();
+            if (FileType.isFile(typeId)) {
+                String src = findSrc(sources);
+                String packSource = constructPackage(file, src);
+                String className = packSource + file.getFileName();
+                try {
+                    clazz = clazzLoader.loadClass(className);
+                    Method[] meth = clazz.getDeclaredMethods();
+                    int j = 0;
+                    while (meth != null && j != meth.length) {
+                        if (isMain(meth[j])) {
+                            // we thought that there is only 1 "main" for the project
+                            mainMeth = meth[j];
+                        }
+                        j++;
+                    }
+                } catch (ClassNotFoundException | NullPointerException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return mainMeth;
+    }
+
+    public boolean isMain(Method method) {
+        if (method.getName().equals("main") && Modifier.isStatic(method.getModifiers())
+                && Modifier.isPublic(method.getModifiers()) && method.getReturnType().equals(Void.TYPE)) {
+            Type[] params = method.getGenericParameterTypes();
+            if (params.length == 1) {
+                if (params[0].getTypeName().equals("java.lang.String[]")) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+
+    public String findSrc(List<File> sources) {
+        String src = "";
+        for (int i = 0; i < sources.size(); i++) {
+            File file = sources.get(i);
+            if (file.getType().getTypeId() == SOURCES) { //typeId = 3 - "sources"
+                src = file.getPath();
+            }
+        }
+        return src;
+    }
+
+    public List<SimpleJavaFileObject> constructResources(List<File> sources) {
         List<SimpleJavaFileObject> userSources = new ArrayList<>();
         for (int i = 0; i < sources.size(); i++) {
             File file = sources.get(i);
-            // typeId = 8 - "class", typeId = 9 - "interface", typeId = 10 - "exception"
-            // typeId = 5 - "runnable", typeId = 11 - "enum", typeId = 12 - "annotation"
             int typeId = file.getType().getTypeId();
-            if (typeId == 8 || typeId == 9 || typeId == 10 || typeId == 5 || typeId == 11 || typeId == 12) {
+            if (FileType.isFile(typeId)) {
                 byte[] userFByte = file.getData();
                 String userFile = "";
                 for (int j = 0; j < userFByte.length; j++) {
                     userFile += (char) userFByte[j];
                 }
-                String s = findSrc(sources);
-                String src = file.getPath();
-                src = src.replaceFirst(s + "/", "");
-                src = src.replace("/", ".");
-                int pos = src.lastIndexOf(file.getFileName());
-                String pack = src.substring(0, pos);
+                String srcFolder = findSrc(sources);
+                String pack = constructPackage(file, srcFolder);
                 String className = file.getFileName() + ".java";
                 SimpleJavaFileObject sjf = null;
                 if (pack != null) {
@@ -84,107 +207,18 @@ public class CompileAndRunBean extends DynamicCompiler {
                 userSources.add(sjf);
             }
         }
-
-        String message = "";
-
-        try {
-            message = compileToClass(userSources);
-        } catch (Exception e) {
-            LOG.error("Exception, can't compile", e);
-        }
-        LOG.info(message);
-        System.out.flush();
-        System.setOut(old);
-        return baos.toString();
+        return userSources;
     }
 
-    //!TODO
-    public String run(String projectHash) {
-        List<File> sources;
-        FileBean fileBean = new FileBean();
-        sources = fileBean.getProjectFiles(projectHash);
-        String mes = "";
-        if (!getMessage().equals("")) {
-            Method mainMeth = findMain(sources, getClassLoader());
-            String[] arguments = {};
-            try {
-                ByteArrayOutputStream baos = new ByteArrayOutputStream();
-                PrintStream printStream = new PrintStream(baos);
-                PrintStream old = System.out;
-                System.setOut(printStream);
-                mainMeth.invoke(null, (Object) arguments);
-                System.out.flush();
-                System.setOut(old);
-                mes = baos.toString();
-            } catch (NullPointerException e) {
-                LOG.error(e.getMessage(), e);
-            } catch (InvocationTargetException e) {
-                LOG.error(e.getMessage(), e);
-            } catch (IllegalAccessException e) {
-                LOG.error(e.getMessage(), e);
-            }
-        }
-        return mes;
+    public String constructPackage(File file, String srcFolder) {
+        String src = file.getPath();
+        src = src.replaceFirst(srcFolder + "/", "");
+        src = src.replace("/", ".");
+        int pos = src.lastIndexOf(file.getFileName());
+        String pack = src.substring(0, pos);
+        return pack;
     }
 
-    public Method findMain(List<File> sources, ClassLoader clazzLoader) {
-        int count = 0;
-        Class clazz = null;
-        Method mainMeth = null;
-        for (int i = 0; i < sources.size(); i++) {
-            File file = sources.get(i);
-            // typeId = 8 - "class", typeId = 9 - "interface", typeId = 10 - "exception"
-            // typeId = 5 - "runnable", typeId = 11 - "enum", typeId = 12 - "annotation"
-            int typeId = file.getType().getTypeId();
-            if (typeId == 8 || typeId == 9 || typeId == 10 || typeId == 5 || typeId == 11 || typeId == 12) {
-                String src = findSrc(sources);
-                String packSource = file.getPath().replaceFirst(src, "").replace("/", ".").replaceFirst(".", "");
-//                String pack = "";
-//                if (!packSource.equals("")) {
-//                    pack = packSource + ".";
-//                }
-                String className = packSource;//pack + file.getFileName();
-                try {
-                    clazz = clazzLoader.loadClass(className);
-                    Method[] meth = clazz.getDeclaredMethods();
-                    int j = 0;
-                    while (meth != null && j != meth.length) {
-                        if (meth[j].getName().equals("main")) {
-                            mainMeth = meth[j];
-                            count++;
-                        }
-                        j++;
-                    }
-                } catch (ClassNotFoundException e) {
-                    LOG.error("ClassNotFoundException", e);
-                } catch (NullPointerException e) {
-                    LOG.error("NullPointerException", e);
-                }
-            }
-        }
-
-        if (count != 1) {
-            try {
-                throw new Exception();
-            } catch (Exception e) {
-                LOG.error("Too many main functions", e);
-            }
-            return null;
-        } else {
-            return mainMeth;
-        }
-    }
-
-    public String findSrc(List<File> sources) {
-        String src = "";
-        for (int i = 0; i < sources.size(); i++) {
-            File file = sources.get(i);
-            if (file.getType().getTypeId() == 3) { //typeId = 3 - "sources"
-                src = file.getPath();
-            }
-        }
-        return src;
-    }
 
     public SimpleJavaFileObject counstructResource(String className, String source) {
         return counstructResource(className, source, DEFAULT_PACKAGE_PREFFIX);
